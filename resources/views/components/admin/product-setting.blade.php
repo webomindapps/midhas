469a3c336d0150bac08c323a629fb6d37b26cd4b
DhruvWebomindapps <138429149+DhruvWebomindapps@users.noreply.github.com>
2025-04-25 11:49:48 +0530

    updated product setting

--- resources/views/components/admin/product-setting.blade.php
+++ resources/views/components/admin/product-setting.blade.php
@@ -1,22 +1,23 @@
-@props(['existing'])
-<div class="row">
-    @php
-        $settings = [
-            'is_new' => 'Is New',
-            'is_featured' => 'Is Featured',
-            'is_best_selling' => 'Is Best Selling',
-            'is_taxable' => 'Is Taxable',
-            'is_outof_stock' => 'Is Out of Stock',
-            'is_comming' => 'Coming Soon',
-        ];
-    @endphp
+@props(['existing' => []])
+@php
+    $existing = (object) $existing;
+    $settings = [
+        'is_new' => 'Is New',
+        'is_featured' => 'Is Featured',
+        'is_best_selling' => 'Is Best Selling',
+        'is_taxable' => 'Is Taxable',
+        'is_outof_stock' => 'Is Out of Stock',
+        'is_comming' => 'Coming Soon',
+    ];
+@endphp
 
+<div class="row">
     @foreach ($settings as $key => $label)
         <div class="col-lg-2">
             <div class="input-check">
                 <input type="checkbox" name="settings[]" id="settings-{{ $key }}" value="{{ $key }}"
                     class="form-check-input"
-                    {{ old('settings') ? (in_array($key, old('settings', [])) ? 'checked' : '') : ($existing->$key ? 'checked' : '') }}>
+                    {{ old('settings') ? (in_array($key, old('settings', [])) ? 'checked' : '') : ($existing->$key ?? false ? 'checked' : '') }}>
                 <label class="form-label" for="settings-{{ $key }}">{{ $label }}</label>
             </div>
         </div>
